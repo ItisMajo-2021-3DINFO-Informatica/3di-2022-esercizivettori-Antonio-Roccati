1f41ba807e97366d71116e015b3de3c2a7271968
Andrea <98151079+Antonio-Roccati@users.noreply.github.com>
2022-03-18 10:44:55 +0100

    Esercizio 2 finito

---
+++ EsercizioRoccati2/EsercizioRoccati2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EsercizioRoccati2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("esercizio 2");
+            const int dimensione = 100;
+            Random generatore = new Random();
+            int[] Vettore = new int[dimensione];
+            for (int i = 0; i < dimensione; i++)
+            {
+                Vettore[i] = generatore.Next(1, 101);
+            }
+            for (int i = 0; i >= 50; i++)
+            {
+                Console.WriteLine(Vettore[i]);
+            }
+        }
+    }
+}
